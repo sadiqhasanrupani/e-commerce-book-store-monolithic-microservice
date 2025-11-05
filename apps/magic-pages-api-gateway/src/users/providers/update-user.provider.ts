213579622a14644ebd11bc762e693f8c4a8e8133
sadiqhasanrupani <sadiqhasanrupani11@gmail.com>
2025-11-05 20:40:22 +0530

    refactor(users): simplify update-user.provider placeholder

--- apps/magic-pages-api-gateway/src/users/providers/update-user.provider.ts
+++ apps/magic-pages-api-gateway/src/users/providers/update-user.provider.ts
@@ -1,22 +1,4 @@
-import { CreateUserDto } from "@app/contract/users/dtos/create-user.dto";
-import { User } from "@app/contract/users/entities/user.entity";
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CreateUserProvider {
-  constructor(
-    /**
-     * Inject userRepository
-     * */
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
-  ) {}
-
-  async createUser(createUserDto: CreateUserDto): Promise<User> {
-    const user = this.userRepository.create(createUserDto);
-
-    return await this.userRepository.save(user);
-  }
-}
+export class UsersService { }
